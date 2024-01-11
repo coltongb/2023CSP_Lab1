7da67f99c57bab7994f7a68a7768ffb1bd45f120
20288 <colton.bates@vtsd.com>
2024-01-11 09:24:45 -0500

    .

--- main.py
+++ main.py
@@ -27,21 +27,48 @@ def setupBox():
     box.forward(630)
     box.hideturtle()
 
-
+srtx = -490
+srty = -300
+endy = -300
+endx = 490
+lines.speed(0)
 # Code for 80 point version goes here
 def v80():
-   lines.goto(-490, -300)
-
+    global srtx, srty, endx,endy
+    lines.penup()
+    lines.goto(srtx, srty)
+    lines.pendown()
+    for shape in range(50):
+        lines.penup()
+        lines.goto(srtx, srty)
+        lines.pendown()
+        lines.goto(endx, endy)
+        srtx += 20
+        endy += 13
 
 
 
 # Code for the 90 point version goes here
 def v90():
     # Calling the 80 point function - don't copy-paste from earlier method!!
+    global srtx
+    global srty
+    global endx
+    global endy
     v80()
+    lines.penup()
+    lines.goto(-srtx, srty)
+    lines.pendown()
+    endy -= 630
+    for shape in range(50):
+        lines.penup()
+        lines.goto(srtx, srty)
+        lines.pendown()
+        lines.goto(-endx, endy)
+        srtx -= 20
+        endy += 13
 
-
-
+v90()
 
 # Code for the 100 point version here
 def v100():
